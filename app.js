d35f90b5410846611fda2a65121eea74cfd3e373
sravan ks <sravansreyas3@gmail.com.com>
2025-07-10 19:28:46 +0530

    basic node+express server

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const port = 5000;
+app.get("/helo", (req, res) => {
+  res.send("test api is working fine");
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
